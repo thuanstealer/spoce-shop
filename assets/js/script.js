472750da96757d63228c327b5d4af3868a553312
Nguyễn Nam Dương <3123560012@sv.sgu.edu.vn>
2024-10-17 20:27:04 +0700

    Main web

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// const imageSlideShow = [
+//     "../image/slide-show/slider_1.png",
+//     "../image/slide-show/slider_2.png"
+// ];
+// let currentIndex = 0;
+
+// function changeImage() {
+//     const slideShowElement = document.getElementById("slide-show");
+//     if (slideShowElement) {
+//         currentIndex = (currentIndex + 1) % imageSlideShow.length;
+//         slideShowElement.src = imageSlideShow[currentIndex];
+//     } else {
+//         console.error("Element with id 'slide-show' not found.");
+//     }
+// }
+
+// setInterval(changeImage, 3000);
